b3201538532a8572b209d7cfdef5c85657e82766
Micha Davis <m.leigh.davis@gmail.com>
2022-02-17 11:20:46 -0800

    completed exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      word: '',
+      nums: nums
+    }
+  }
+
+    handleName = (event) => {
+      this.setState({
+        name: event.target.value
+      })
+    }
+
+    handleWord = (event) => {
+      this.setState({
+        word: event.target.value
+      });
+    }
+
+    handleSelect = (event) => {
+      if(event.target.value === 'even'){
+        let newNums = nums.filter(num => num % 2 === 0);
+        this.setState({ nums: newNums });
+      }
+      else if(event.target.value === 'odd'){
+        let newNums = nums.filter(num => num % 2 !== 0);
+        this.setState({ nums: newNums });
+      }
+      else{
+        this.setState({ nums: nums })
+      }
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+
+      this.setState({
+        submitName: event.target.name.value,
+        submitWord: event.target.name.value,
+        submitSelected: event.target.name.value
+      });
+    }
+
+  render() {
+
+    let listItems = this.state.nums.map(listItem => (
+      <li>{listItem}</li>
+    ))
+
+    return (
+      <>
+        <h1>In-FORM-Ed {this.state.name}</h1>
+        <h3>{this.state.word}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+
+            <legend>Deep Ponderings</legend>
+
+            <label>Name
+              <input name="name" type="text" onInput={this.handleName} />
+            </label>
+
+            <label htmlFor="word">Word</label>
+            <input name="word" id="word" type="text" onChange={this.handleWord} />
+
+            <select name="selected" onChange={this.handleSelect}>
+              <option value="all">All</option>
+              <option value="even">Even</option>
+              <option value="odd">Odd</option>
+            </select>
+            <button type="submit">Submit</button>
+          </fieldset>
+        </form>
+
+        <ul>
+          {listItems}
+        </ul>
+      </>
+    );
+  }
 }
-
 export default App;
